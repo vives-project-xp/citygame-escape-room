92e35aff62cb25b38d84d8831688e76401bd29d5
Nico De Witte <nico.dewitte@vives.be>
2017-05-03 12:58:54 +0200

    Add float header for DBL_MAX

--- src/gameelements/mission/LocationMission/LocationMission.cpp
+++ src/gameelements/mission/LocationMission/LocationMission.cpp
@@ -1,7 +1,8 @@
 #include "LocationMission.h"
+#include <cfloat>
 
 LocationMission::LocationMission(std::string name, int id, std::string description, int typeId): Mission(name, id, description, typeId){
-}    
+}
 
 int LocationMission::getIndexOfClosestArea(){
     double distance = DBL_MAX;
@@ -11,11 +12,11 @@ int LocationMission::getIndexOfClosestArea(){
         if(tempDist < distance){
             distance = tempDist;
             index = i;
-        } 
+        }
     }
     return index;
 }
-    
+
 void LocationMission::debug(Serial * serial){
     serial->printf("Target location: %5.6f , %5.6f\n", getLatitude(), getLongitude());
     serial->printf("Radius: %f \n", getRadius());
@@ -30,26 +31,25 @@ bool LocationMission::isComplete(){
 double LocationMission::getLatitude(){
     return locations[getIndexOfClosestArea()]->getLatitude();
 }
-    
+
 double LocationMission::getLongitude(){
     return locations[getIndexOfClosestArea()]->getLongitude();
 }
-    
+
 double LocationMission::getRadius(){
     return locations[getIndexOfClosestArea()]->getRadius();
 }
-  
-   
+
+
 void LocationMission::addTargetLocation(Location * location){
     locations.push_back(location);
 }
-    
+
 double LocationMission::getDistanceToTarget(){
     return locations[getIndexOfClosestArea()]->getDistanceToTarget(this->board);
 }
 
 void LocationMission::toLcd(){
-    this->board->lcd->setMissionScreen(getDescription(),(int)getDistanceToTarget(),(int)getRadius());  
+    this->board->lcd->setMissionScreen(getDescription(),(int)getDistanceToTarget(),(int)getRadius());
     this->board->leds->reactToDistance((int)getDistanceToTarget(), (int)getRadius());
 }
-    
